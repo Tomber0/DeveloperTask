8b9a2b081067a64d24a365bfb0d9b2218c790ff1
Aleksey.Zaitsau <aleseyzaicev@gmail.com>
2023-12-12 01:56:08 +0300

    added fourth Vehicle class

---
+++ DeveloperTask/Task2/Carriage.cs
@@ -0,0 +1,15 @@
+﻿using DeveloperTask.Task1;
+
+namespace DeveloperTask.Task2
+{
+    internal class Carriage : Vehicle
+    {
+        public Carriage():base()
+        {
+        }
+
+        public Carriage(float topSpeed, string name, int passengerCount) : base(topSpeed, name, passengerCount)
+        {
+        }
+    }
+}
